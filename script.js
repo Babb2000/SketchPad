11c8b3a101c8cc80d819b4d27def229067799643
Babb Getachew <babbgetachew@gmail.com>
2022-03-01 12:22:23 -0500

    Updated page view

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-//Function to call for dynamic sketchpad size
 function makeGrid()
 {
+    let tblr = document.getElementById("grid");
+
+    for(let i = 0; i < 11; i++)
+    {
+        let tblrw = document.createElement('tr');
+        let tblcol = document.createElement('td');
+        
+        tblrw.id = `row + ${i}`;
+        tblr.appendChild(tblrw);
+
+        for(let j = 0; j < 11; j++)
+        {
+            tblrw.appendChild(tblcol);
+        }
+    }
+
     
 }
