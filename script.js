e43b12a804ae7e290cf2d4eeade7be13cb8f6963
Babb Getachew <babbgetachew@gmail.com>
2022-03-02 09:56:46 -0500

    added dom functionality to the submit button

--- script.js
+++ script.js
@@ -1,17 +1,52 @@
-function makeGrid(){
-    let gridContainer = document.getElementById('grid');
+const widthValue = 750;
+const lenValue = 750;
 
-    for(let i = 0; i < 16; i++)
+function makeGrid(){
+    
+    let gridContainer = document.getElementById('grid');
+    
+    let num1 = document.getElementById('num1').value;
+    let num2 = document.getElementById('num2').value;
+
+    const finOne = parseInt(num1);
+    const finTwo = parseInt(num2);
+ 
+    let newWid = (widthValue/finOne); //750 divided by the user entered width value
+    let newLen = (lenValue/finTwo);   //750 divided by the user entered length value
+
+    let toTxtwid = newWid.toString();
+    let toTxtlen = newLen.toString();
+
+    let cssWidth = (toTxtwid + "px");
+    let cssLength = (toTxtlen + "px");
+
+    document.getElementById('grid').style.gridTemplateColumns = `repeat(${finOne}, ${cssWidth})`;
+    document.getElementById('grid').style.gridAutoRows = `${cssLength}`;
+  
+    let currentDiv = document.getElementById('grid');
+    const myDivChildNodes = currentDiv.children;
+
+    for(let q = 0; q < myDivChildNodes.length; q++)
+    {
+      myDivChildNodes[q].style.width = `${cssWidth}`;
+      myDivChildNodes[q].style.length = `${cssLength}`;
+    }
+
+    for(let i = 0; i < num1; i++)
     {
     
     let myRows = document.createElement('div');
     gridContainer.appendChild(myRows);
     
-       for(let j = 0; j < 16; j++)
+       for(let j = 1; j < num2; j++)
        {
          let myCol = document.createElement('div');
          gridContainer.appendChild(myCol);
        }
 
+      }
 }
-}
+
+
+
+
